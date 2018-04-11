f3dbd82884270e54f25775bf142586bd3330e55a
Gandhi Jagasia <gandhi.jagasia@brightscope.com>
2018-04-11 13:37:09 -0700

    Some stuff for pod people

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,5 +1,26 @@
 import * as template from './app.template.html';
 
 export const appComponent = {
-  template
+  template,
+  controller($http, $log) {
+    const food = this;
+    const clickMessage = '';
+
+    // Called when component is ready, see below
+    food.$onInit = () => {
+      $http.get('http://localhost:8000/api/food/?t=pie').then(response => {
+        $log.log(response.data);
+        food.pies = response.data;
+      });
+      $http.get('http://localhost:8000/api/food/?t=sandwich').then(response => {
+        $log.log(response.data);
+        food.sandwich = response.data;
+      });
+    };
+    food.clickMe = (foodName) => {
+      $log.log(foodName);
+    };
+  }
 };
+
+
